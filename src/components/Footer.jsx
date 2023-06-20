e964dfec97165bb3b03ca9e15c85f91c8e972019
OtajonMakxmasoliyev <otajonmakxmasoliyev3112@gmail.com>
2023-06-20 16:22:31 +0500

    O'n birinchi deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,70 +1,87 @@
-import "../style/footer.scss"
-import { FaFacebookF, FaYoutube } from "react-icons/fa"
-import { BsInstagram, BsTelegram, BsTwitter } from "react-icons/bs"
-import TweenScroll from "./TweenScroll"
-import { Tween } from "react-gsap"
-import { useEffect, useState } from "react"
-import { useTranslation } from "react-i18next"
-
-
-
-
+import "../style/footer.scss";
+import { FaFacebookF, FaYoutube } from "react-icons/fa";
+import { BsInstagram, BsTelegram, BsTwitter } from "react-icons/bs";
+import TweenScroll from "./TweenScroll";
+import { Tween } from "react-gsap";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-    const { t } = useTranslation()
-    function ScrollTop(url){
-        console.log(document.querySelector("footer").scrollTop);
-    }
+  const { t } = useTranslation();
+  function ScrollTop(url) {
+    console.log(document.querySelector("footer").scrollTop);
+  }
 
-    const [height, setHeight] = useState()
+  const [height, setHeight] = useState();
 
-    const style = {
+  const style = {
+    width: "30px",
+    height: "30px",
+  };
+  useEffect(() => {
+    let screenHeight = window.innerHeight;
+    setHeight(screenHeight);
+  }, []);
+  return (
+    <footer className="footer">
+      <div className="container  footer_block">
+        <div className="logo">
+          <img src={require("../images/BEAR LOGO.png")} />
+        </div>
+        <div className="center">
+          <h1>{t("about.footer")}</h1>
+        </div>
+        <dl>
+          <dt>{t("header.Links")}</dt>
+          <dd>
+            <a href="#showcase"> {t("header.Home")}</a>
+          </dd>
+          <dd>
+            <a href="#About"> {t("header.AboutUs")}</a>
+          </dd>
+          <dd>
+            <a href="#Benefits">{t("header.Benefits")}</a>
+          </dd>
 
-        width: "30px",
-        height: "30px"
-    }
-    useEffect(() => {
-        let screenHeight = window.innerHeight
-        setHeight(screenHeight)
+          <dd>
+            <a href="#Step">{t("header.start")}</a>
+          </dd>
+          <dd>
+            <a href="#Cooperation">{t("header.Cooperation")}</a>
+          </dd>
 
-    }, [])
-    return (
-
-
-        <footer className="footer" >
-            <div className="container  footer_block">
-                <div className="logo">
-                    <img src={ require("../images/BEAR LOGO.png") } />
-                </div>
-                <dl>
-                    <dt>{t("header.Links")}</dt>
-                    <dd>
-                        <a  href="#Shocase">{ t("header.Home") }</a>
-                    </dd>
-                    <dd>
-                        <a href="#About"> { t("header.AboutUs") }</a>
-                    </dd>
-                    <dd>
-                        <a href="#Benefits">{ t("header.Benefits") }</a>
-                    </dd>
-                    <dd>
-                        <a href="#Contact">{ t("header.Contact") }</a>
-                    </dd>
-                </dl>
-               
-            </div>
-            <div className="line"></div>
-            <div className="container footer_bottom">
-                
-                <div className="icons">
-                    <a href="https://www.facebook.com/profile.php?id=100094026783106"><FaFacebookF className="facebook" width={ style.width } height={ style.height } style={ { ...style, color: "#fff" } } /></a>
-                   <a href="https://www.youtube.com/@Bearpartner"> <FaYoutube className="youtube" style={ { ...style } } /></a>
-                    <a href="https://www.instagram.com/bear_partner/"><BsInstagram className="instagram" style={ { ...style } } /></a>
-                    <a href="https://t.me/Bearpartner"><BsTelegram className="telegram" style={ { ...style } } /></a>
-                </div>
-            </div>
-        </footer>
-
-    )
-}
-export default Footer
+          <dd>
+            <a href="#Feedback">{t("header.feedback")}</a>
+          </dd>
+          <dd>
+            <a href="#Contact">{t("header.Contact")}</a>
+          </dd>
+        </dl>
+      </div>
+      <div className="line"></div>
+      <div className="container footer_bottom">
+        <div className="icons">
+          <a href="https://www.facebook.com/profile.php?id=100094026783106">
+            <FaFacebookF
+              className="facebook"
+              width={style.width}
+              height={style.height}
+              style={{ ...style, color: "#fff" }}
+            />
+          </a>
+          <a href="https://www.youtube.com/@Bearpartner">
+            {" "}
+            <FaYoutube className="youtube" style={{ ...style }} />
+          </a>
+          <a href="https://www.instagram.com/bear_partner/">
+            <BsInstagram className="instagram" style={{ ...style }} />
+          </a>
+          <a href="https://t.me/Bearpartner">
+            <BsTelegram className="telegram" style={{ ...style }} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
